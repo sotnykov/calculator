8df69041baf780aece2668408bd7904d10335d0d
Andrii Sotnykov <andrew.sotnikov1437@gmail.com>
2023-10-07 14:45:51 +0200

    added NaN checker and alert replaced

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
-let calc = prompt('What do you want to do? (Addition, Subtraction, Multiplication, Division)');
+let calc;
 
-let firstPrompt = prompt('Write the first number:');
+do {
+  calc = prompt('What do you want to do? ( + , - , * , / )')
 
-let secondPrompt = prompt('Write the second number');
+  if (calc !== '+' && calc !== '-' && calc !== '*' && calc !== '/') {
+    alert('Write, please +, -, * or /');
+  }
+
+} while (calc !== '+' && calc !== '-' && calc !== '*' && calc !== '/');
+
+let firstPrompt;
+
+do {
+  firstPrompt = prompt('Write the first number:')
+
+  if (isNaN(firstPrompt)) {
+    alert(firstPrompt + ` is not a number!`)
+  }
+
+} while (isNaN(firstPrompt));
+
+let secondPrompt;
+
+do {
+  secondPrompt = prompt('Write the first number:')
+
+  if (isNaN(secondPrompt)) {
+    alert(secondPrompt + ` is not a number!`)
+  }
+  
+} while (isNaN(secondPrompt));
 
 const firstNum = +firstPrompt;
 
@@ -12,10 +39,8 @@ if (calc === '+') {
   alert(`${firstPrompt} + ${secondPrompt} = ` + (firstNum + secondNum))
 } else if (calc === '-') {
   alert(`${firstPrompt} - ${secondPrompt} = ` + (firstNum - secondNum))
-} else if (calc === '*' ) {
+} else if (calc === '*') {
   alert(`${firstPrompt} * ${secondPrompt} = ` + (firstNum * secondNum))
 } else if (calc === '/') {
   alert(`${firstPrompt} / ${secondPrompt} = ` + (firstNum / secondNum))
-} else {
-  alert('Write, please +, -, * or /')
 }
